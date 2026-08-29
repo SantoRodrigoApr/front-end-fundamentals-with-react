c7beab3bdd5f04a6c978e49a073432b77be40a00
SantoRodrigoApr <santorodrigoapr@outlook.com>
2026-08-29 18:05:04 +1000

    Added updated and comprehenseve diagram including:  - Events  - Hooks Life-Cycle (useEffect) Customized Contador and MeuNome to apply in-context knowledge.

--- src/app/nivel-1/page.tsx
+++ src/app/nivel-1/page.tsx
@@ -2,7 +2,7 @@ import {Contador} from "@/src/components/Contador";
 
 const Page = () => (
     <div>
-        <h1 className="text-4xl font-bold">Página Nivel 1</h1>
+        <h1 className="text-4xl font-bold italic">Página Nivel 1</h1>
         <Contador />
     </div>
 );
--- src/components/Contador.tsx
+++ src/components/Contador.tsx
@@ -1,12 +1,53 @@
 "use client";
 
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {Button} from "@/src/components/Button";
 
 export const Contador = () => {
-    const [contador] = useState(0);
+    const [contador, setContador] = useState(0);
+    const [coisa, setCoisa] = useState('');
+
+    useEffect(() => {
+        console.log("Comtador atualizado");
+    }, [contador]);
+
+
+    useEffect(() => {
+        console.log("Coisa atualizado");
+    }, [coisa]);
 
     return <div>
         <h2 className="text-2xl">Contador</h2>
         <p>Número atual: {contador}</p>
+        <div className="flex grid-cols-2 gap-x-2 gap-y-2">
+            <Button
+                    onClick={() => {
+                        console.log(setContador(contador + 1));
+                    }}
+            >+1</Button>
+            <Button
+                    onClick={() => {
+                        setContador((c: number): number => c + 2);
+                        setContador((c: number): number => c + 1);
+                    }}
+            >+3</Button>
+            <Button
+                    onClick={() => {
+                        setContador((c: number): number => c - 2);
+                    }}
+            >-2</Button>
+            <button className="border rounded grid px-5.5 py-1"
+            onClick={() => {setContador( 0);}}>
+                Reset
+            </button>
+        </div>
+        <div className="flex py-2 gap-x-2">
+            <input value={coisa} onChange={
+                (e) => setCoisa(e.target.value)
+            } className="border px-10 border-gray-500 rounded"/>
+            <Button
+                onClick={() => {setCoisa('')}}
+            >Limpar</Button>
+        </div>
     </div>
 }
